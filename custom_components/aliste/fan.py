2339328b97344302ff1564adf4f0283786552603
Mudit Bansal <muditbansal95@gmail.com>
2025-09-16 23:19:30 +0530

    v1.0.6

--- custom_components/aliste/fan.py
+++ custom_components/aliste/fan.py
@@ -1,5 +1,5 @@
 import logging
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.fan import FanEntity, FanEntityFeature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -9,7 +9,7 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities: AddEntitiesCallback):
-    """Set up Aliste switches."""
+    """Set up Aliste fans."""
     api = hass.data[DOMAIN][entry.entry_id]
 
     # Fetch houses - take first house
@@ -24,7 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities: AddE
     rooms_resp = api.get_rooms(house_id)
     rooms = rooms_resp.get("rooms", [])
 
-    switches = []
+    fans = []
 
     for room in rooms:
         room_id = room["roomId"]
@@ -33,51 +33,73 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities: AddE
 
         for appliance in appliances:
             appliance_type = appliance.get("applianceType")
-            if appliance_type != 20:  # Not fan type
-                switches.append(AlisteSwitch(api, appliance, room))
+            if appliance_type == 20:  # Fan type
+                fans.append(AlisteFan(api, appliance, room))
 
-    async_add_entities(switches, update_before_add=True)
+    async_add_entities(fans, update_before_add=True)
 
 
-class AlisteSwitch(SwitchEntity):
+class AlisteFan(FanEntity):
     def __init__(self, api: AlisteAPI, appliance: dict, room: dict):
         self._api = api
         self._appliance = appliance
         self._room = room
-        self._is_on = int(appliance.get("state", "0")) > 0
-        self._attr_name = appliance.get("name", "Aliste Switch")
-        self._unique_id = f"aliste_switch_{appliance.get('deviceId')}_{appliance.get('switchId')}"
+        self._speed = int(appliance.get("state", "0"))
+        self._attr_name = appliance.get("name", "Aliste Fan")
+        self._unique_id = f"aliste_fan_{appliance.get('deviceId')}_{appliance.get('switchId')}"
 
     @property
-    def is_on(self):
-        return self._is_on
+    def unique_id(self):
+        return self._unique_id
 
     @property
     def name(self):
         return self._attr_name
 
     @property
-    def unique_id(self):
-        return self._unique_id
+    def speed(self):
+        speed_percent = self._speed
+        if speed_percent == 0:
+            return "off"
+        elif speed_percent <= 25:
+            return "low"
+        elif speed_percent <= 50:
+            return "medium"
+        elif speed_percent <= 75:
+            return "high"
+        else:
+            return "max"
+
+    @property
+    def speed_list(self):
+        return ["off", "low", "medium", "high", "max"]
+
+    @property
+    def supported_features(self):
+        return FanEntityFeature.SET_SPEED
 
     async def async_turn_on(self, **kwargs):
-        await self.hass.async_add_executor_job(
-            self._api.execute_action,
-            self._appliance["deviceId"],
-            str(self._appliance["switchId"]),
-            "100"
-        )
-        self._is_on = True
-        self.schedule_update_ha_state()
+        await self.async_set_speed("max")
 
     async def async_turn_off(self, **kwargs):
+        await self.async_set_speed("off")
+
+    async def async_set_speed(self, speed):
+        speed_map = {
+            "off": "0",
+            "low": "25",
+            "medium": "50",
+            "high": "75",
+            "max": "100",
+        }
+        command_value = speed_map.get(speed, "0")
         await self.hass.async_add_executor_job(
             self._api.execute_action,
             self._appliance["deviceId"],
             str(self._appliance["switchId"]),
-            "0"
+            command_value,
         )
-        self._is_on = False
+        self._speed = int(command_value)
         self.schedule_update_ha_state()
 
     async def async_update(self):
@@ -88,5 +110,5 @@ class AlisteSwitch(SwitchEntity):
                 appl["deviceId"] == self._appliance["deviceId"]
                 and appl["switchId"] == self._appliance["switchId"]
             ):
-                self._is_on = int(appl.get("state", "0")) > 0
+                self._speed = int(appl.get("state", "0"))
                 break
